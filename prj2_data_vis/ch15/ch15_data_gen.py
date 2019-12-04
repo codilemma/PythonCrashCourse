1e41dd9ad41342dc3eac9498780b291464153b1c
CH <cody.holthus@gmail.com>
2019-12-04 10:28:33 -0800

    Chapter 15 Complete!

--- prj2_data_vis/ch15/ch15_data_gen.py
+++ prj2_data_vis/ch15/ch15_data_gen.py
@@ -135,6 +135,8 @@ plt.show()
 # ---------------------------------------
 # Rolling Dice with Plotly
 # ---------------------------------------
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
 from die import Die
 
 # Create a D6. - six sided die
@@ -146,4 +148,88 @@ for roll_num in range(100):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analyze the results
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title':'Result'}
+y_axis_config = {'title':'Frequency of Result'}
+my_layout = Layout(title='Reults of rolling one D6 1000 times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data':data,'layout':my_layout}, filename='d6.html')
+
+# ---------------------------------------
+# Rolling Two Dice with Plotly
+# ---------------------------------------
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from die import Die
+
+# Create two D6 dice
+die_1 = Die()
+die_2 = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results
+frequencies
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2,max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+x_values = list(range(2,max_result+1))
+data = [Bar(x=x_values,y=frequencies)]
+
+x_axis_config = {'title':'Result','dtick':1} # dtick:1 labels every tick mark
+y_axis_config = {'title':'Frequency of Result'}
+my_layout = Layout(title='Results of rolling two D6 dice 1000 times',
+                   xaxis=x_axis_config,yaxis=y_axis_config)
+offline.plot({'data':data,'layout':my_layout}, filename='d6_d6.html')
+
+# ---------------------------------------
+# Rolling Dice of Different Sizes
+# ---------------------------------------
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from die import Die
+
+#Create a D6 and a D10
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls, and store results in a list
+results = []
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll()
+    results.append(results)
+
+# Analyze the results
+frequencies
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2,max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+x_values = list(range(2,max_result+1))
+data = [Bar(x=x_values,y=frequencies)]
+
+x_axis_config = {'title':'Result','dtick':1} # dtick:1 labels every tick mark
+y_axis_config = {'title':'Frequency of Result'}
+my_layout = Layout(title='Results of rolling a D6 and a D10 5000 times',
+                   xaxis=x_axis_config,yaxis=y_axis_config)
+offline.plot({'data':data,'layout':my_layout}, filename='d6_d10.html')
