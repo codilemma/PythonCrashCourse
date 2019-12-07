833ca6cf91a88fc1a9d77cb656b6a297ea99d277
CH <cody.holthus@gmail.com>
2019-12-06 16:21:04 -0800

    Chapter 16 Complete

--- prj2_data_vis/ch16/ch16_data_download.py
+++ prj2_data_vis/ch16/ch16_data_download.py
@@ -206,11 +206,11 @@ plt.show()
 import json
 
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'data/readable_eq_data.json'
+readable_file = 'data/readable_eq_data_30day.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data,f,indent=4)
 
@@ -218,10 +218,15 @@ with open(readable_file, 'w') as f:
 # -------------------------------------------------
 # Mapping global Data Sets: JSON Format
 # -------------------------------------------------
+# https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/recent_eq_data_30days.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -229,11 +234,35 @@ with open(filename) as f:
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts)) # Total number of earthquakes
 
-# Extracting the magnitutes
-mags = []
+# Extracting the magnitutes and locations
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    mags.append(mag)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
-# Extracting location data.
+# Map the earthquakes.
+# data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type':'scattergeo',
+    'lon':lons,
+    'lat':lats,
+    'text':hover_texts,
+    'marker':{
+        'size':[5*mag for mag in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Magnitude'},
+    },
+}]
+
+# To see other available colorscdales
+# for key in colors.PLOTLY_SCALES.keys():
+#   print(key)
+my_layout = Layout(title=all_eq_data['metadata']['title'])
+
+fig = {'data':data,'layout':my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
 
