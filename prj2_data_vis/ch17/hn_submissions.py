8841e6d0f77b289b1c9dc506dd76a34f71ccd77f
CH <cody.holthus@gmail.com>
2019-12-29 19:34:26 -0800

    Progress

---
+++ prj2_data_vis/ch17/hn_submissions.py
@@ -0,0 +1,44 @@
+#https://hacker-news.firebaseio.com/v0/topstories.json
+
+from operator import itemgetter
+
+import requests
+import json
+
+# Make an API call and store the response.
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+
+# Explore the structure of the data
+readable_file = 'data/readable_hn_top_stories.json'
+with open(readable_file, 'w') as f:
+    json.dump(submission_ids,f,indent=4)
+
+for submission_id in submission_ids[:30]:
+    # Make a serate API call for each submission.
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    print(f"id:{submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    # Build a dictionary for each article.
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link':f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['descendants'],
+    }
+    submission_dicts.append(submission_dict)
+
+# Sort to place the most commented stories first
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
+
